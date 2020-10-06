df5fac93a3efc0ca4ebf2656a6e7437d105fb1ae
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2020-10-06 16:27:15 +0100

    settings modification

---
+++ portfolio/database.py
@@ -0,0 +1,22 @@
+import dj_database_url
+from decouple import config
+
+def generate_database(debug):
+    if debug == True:
+        return {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': config('DB_NAME'),
+                'USER': config('DB_USER'),
+                'PASSWORD': config('DB_PASSWORD'),
+                'HOST': config('DB_HOST'),
+                'PORT': config('DB_PORT', cast=int),
+            }
+        }
+    else:
+        return {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'DATABASE_URL': config('DATABASE_URL')
+            }
+        }
